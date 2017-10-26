2b6277b225253ba89ced8f57f4902a553524634e
jengleberg <jeffengleberg@hotmail.com>
2017-10-25 20:55:05 -0600

    homework

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { InternalDefensesModule } from './internal-defenses/internal-defenses.module'
+import { TieFighterComponent } from './tie-fighter/tie-fighter.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TieFighterComponent
+  ],
+  imports: [
+    BrowserModule,
+    InternalDefensesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
